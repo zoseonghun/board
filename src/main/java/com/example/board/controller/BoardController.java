0f08f1046ed80905cc9da27fa4983987cd39edcc
seonghun <nuhgnoesoj@gmail.com>
2025-06-10 20:30:59 +0900

    게시글 작성화면

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -3,10 +3,16 @@ package com.example.board.controller;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
 }
