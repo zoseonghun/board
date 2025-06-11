e54cf567e73028353b350eebb8ad5c2adbe616f1
seonghun <nuhgnoesoj@gmail.com>
2025-06-11 23:59:55 +0900

    게시글 조회 화면 구현

--- src/main/java/com/example/board/dto/BoardDTO.java
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -4,21 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
+@Getter
+@Setter
 @ToString
 public class BoardDTO {
-
     private Long id;
-
     private String boardWriter;
-
     private String boardPass;
-
     private String boardTitle;
-
     private String boardContents;
-
     private int boardHits;
-
     private String createdAt;
+
 }
